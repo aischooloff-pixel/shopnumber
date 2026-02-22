6f70ba5dc7f717b6d81a2ecb084ef09cff3144f3
aischooloff-pixel <aischool.off@gmail.com>
2026-02-22 19:53:17 +0300

    Update config.py

--- src/shop_bot/config.py
+++ src/shop_bot/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8', extra='ignore')
 
-    telegram_bot_token: str
+    telegram_bot_token: 8596061120:AAEF7d2C8fZbLO6MfBWFFXDvr6y_US6cMyU
 
     tiger_api_key: str
     tiger_base_url: str = 'https://api.tigersms.org/stubs/handler_api.php'
